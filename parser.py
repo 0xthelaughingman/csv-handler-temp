5e1ba414be3958a54bb6c5c0751b17438b2ec928
TheLaughingMan <theelaughingman@protonmail.com>
2025-06-28 01:11:03 +0530

    init

---
+++ parser.py
@@ -0,0 +1,63 @@
+import pandas as pd
+
+df = pd.read_csv("download-my-queries.csv", header=None, names=range(19424), low_memory=False)
+
+print(df)
+
+lines = 0
+
+for index, row in df.iterrows():
+    print("Current Row:", index)
+
+    if index==0:
+        continue
+
+    item_counter=0
+    query_id = None
+    query_title = None
+    query_string = ""
+
+    for col_name, value in row.items():
+
+        if item_counter == 0:
+            query_id = value
+            item_counter += 1
+            continue
+
+        elif item_counter == 1:
+            query_title = value
+            item_counter +=1
+            continue
+
+        else:
+            # print(col_name)
+            if pd.isnull(value):
+                break
+            else:
+                query_string = query_string + str(value)
+
+    print("INDEX: ", index, " ID:", query_id, " TITLE: ", query_title, " QUERY_STRING: ", query_string)
+
+'''
+NEXT STEP(s):
+    - OUTPUT each csv row into a beautiful/formatted/readable textfile
+    - Attempt to format the only space/no whitespace type info sql string into actual SQL-formatted string...
+    
+    SAMPLE:
+        ===============================================
+        ===============================================
+        QUERY_ID: <query_id>
+        QUERY_TITLE: <query_title>
+        QUERY_STRING:
+        <query_string>
+        
+        
+        ===============================================
+        ===============================================
+        QUERY_ID: <query_id>
+        QUERY_TITLE: <query_title>
+        QUERY_STRING:
+        <query_string>
+'''
+
+
